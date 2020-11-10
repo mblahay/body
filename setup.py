d3d34064120b34856497bc838a6a6d6fda906139
Michael Blahay <mblahay@gmail.com>
2020-11-10 12:11:00 -0500

    Adding a basic setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "body_tool",
+    version = "0.9",
+    author = "Michael Blahay",
+    author_email = "mblahay@gmail.com",
+    description = ("Tool for cropping files"),
+    license = "MIT",
+    keywords = "example documentation tutorial",
+    url = "https://github.com/mblahay/body",
+    packages=['an_example_pypi_project', 'tests'],
+    long_description='Tool for cropping the contents for files.',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
+)
